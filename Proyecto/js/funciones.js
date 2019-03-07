279e90b007708876963e7fd66ce98e920b5a42b4
Edgar Escobedo <edgar.escobedo@introid.com>
2019-03-06 21:57:59 -0600

    CRUD profesores funcionando

--- Proyecto/js/funciones.js
+++ Proyecto/js/funciones.js
@@ -13,6 +13,33 @@ function grabar(opc){
 			document.getElementById("txtNombre").focus();
 			return false;
 		}
+		document.getElementById("frmAddEspecialidades").submit();
+		break;
+
+	case 'profesores':
+		document.getElementById("txtOpc").value = 'add';
+		if(document.getElementById("txtClave").value == '') {
+			alert('Error en los datos reviselos!');
+			document.getElementById("txtClave").focus();
+			return false;
+		}
+		if (document.getElementById("txtNombre").value == '') {
+			alert('Error en los datos reviselos!');
+			document.getElementById("txtNombre").focus();
+			return false;
+		}
+		if (document.getElementById("txtPaterno").value == '') {
+			alert('Error en los datos reviselos!');
+			document.getElementById("txtPaterno").focus();
+			return false;
+		}
+		if (document.getElementById("txtMaterno").value == '') {
+			alert('Error en los datos reviselos!');
+			document.getElementById("txtMaterno").focus();
+			return false;
+		}
+		document.getElementById("frmAddProfesores").submit();
+		break;
 	}
-	document.getElementById("frmAddEspecialidades").submit();
+	
 }
---
+++ Proyecto/profesores/updProfesores.php
@@ -0,0 +1,99 @@
+<?php
+
+session_start();
+  if(empty($_SESSION['usr'])){
+    echo "Debe auteniticarse";
+		exit();
+  }
+  
+  include '../bd/conexion.php';
+
+  //obtenet el id para recuperar el registro correspondiente
+  $id = $_GET['id'];
+
+  //obtener la coleccion de registros que corresponden al id enviad
+  $strQry = "SELECT * FROM profesor WHERE id=$id;";
+
+  //ejecutar la consulta
+  $tablaBD = mysqli_query($link, $strQry);
+
+  //sacar los datos de la tabla de registros intermedios
+  $registro = mysqli_fetch_array($tablaBD);  
+  $clave = $registro['clave'];
+  $nombre = $registro['nombre'];
+  $paterno = $registro['paterno'];
+  $materno = $registro['materno'];
+
+
+  //contruir el html de la interface para la opcion de modificar
+  echo "
+  <html>
+  <head>
+  	<title></title>
+  	<script type='text/javascript'>
+  		function enviar(opc){
+  			switch(opc){
+  				case 'upd':
+  					document.getElementById('txtOpc').value = 'upd';
+  					document.getElementById('txtId').value = '".$id."';
+  					document.getElementById('frmUpdProfesores').submit();
+  				break;
+
+  				case 'del':
+  					document.getElementById('txtOpc').value = 'del';
+  					document.getElementById('txtId').value = '".$id."';
+  					document.getElementById('frmUpdProfesores').submit();
+  				break;
+
+  				case 'regresar':
+  					window.location.href = './shwProfesores.php';
+  				break;
+  			}
+  			
+  		}
+  	</script>
+  </head>
+  <body>
+  	<form  name='frmUpdProfesores' id='frmUpdProfesores' action='./qryProfesores.php' method='POST'>
+  		<table align='center' width='400' border='0'>
+  			<tr height='100'>
+  				<td colspan='2' align='center'>
+  					<b>Modificando Profesores</b>
+  				</td>
+  			</tr>
+  			<tr>
+  				<td align='right' width='200'>Clave:&nbsp&nbsp</td>
+  				<td>$clave</td>
+  			</tr>
+  			<tr>
+                <td align='right'>Nombre:&nbsp&nbsp</td>
+                <td><input type='text' name='txtNombre' id='txtNombre' value='$nombre' autofocus></td>
+            </tr>
+            <tr>
+  				<td align='right'>Nombre:&nbsp&nbsp</td>
+  				<td><input type='text' name='txtPaterno' id='txtPaterno' value='$paterno' autofocus></td>
+            </tr>
+            <tr>
+  				<td align='right'>Nombre:&nbsp&nbsp</td>
+  				<td><input type='text' name='txtMaterno' id='txtMaterno' value='$materno' autofocus></td>
+  			</tr>
+  			<tr height='80'>
+  				<td colspan='2' align='center'>
+  					
+  					<input type='hidden' name='txtOpc' id='txtOpc'>
+  					<input type='hidden' name='txtId' id='txtId'>
+
+  					<input type='button' name='btnGrabar' id='btnGrabar' value='Grabar' style='width: 100px' onClick='enviar(\"upd\")'>
+  					
+  					<input type='button' name='btnEliminar' id='btnEliminar' value='Eliminar' style='width: 100px' onClick='enviar(\"del\")'>
+  					
+  					<input type='button' name='btnRegresar' id='btnRegresar' value='Regresar' style='width: 100px' onClick='enviar(\"regresar\")'>
+  				</td>
+  			</tr>
+  		</table>
+  		
+  	</form>
+  </body>
+  </html>
+  ";
+?>
